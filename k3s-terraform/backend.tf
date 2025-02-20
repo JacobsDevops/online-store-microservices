8f546cf0f04e803415c4f50f5c18a2564b3e2bcc
Gokdeniz <gokdeniz@Gokdeniz-Dizustu-Bilgisayar.local>
2025-02-20 11:06:48 +0300

    backen.tf modified for s3 bucket

--- k3s-terraform/backend.tf
+++ k3s-terraform/backend.tf
@@ -1,4 +1,14 @@
+#terraform {
+ # backend "http" {
+  #}
+#}
+
 terraform {
-  backend "http" {
+  backend "s3" {
+    bucket = "k3s-backend"          # new andy backend name
+    key    = "terraform.tfstate"        # Path for the state file in the bucket
+    region = "us-east-1"                # AWS region where your bucket is located
+    # encryption is disabled
+    # dynamodb_table is not used
   }
 }
